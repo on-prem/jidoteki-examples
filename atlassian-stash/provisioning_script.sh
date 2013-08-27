06c01b6e251c61a7e3593955183a27ec63832077
Alex Williams <awilliams@alexwilliams.ca>
2013-08-27 19:24:42 +0000

    Add our provisioning script

---
+++ atlassian-stash/provisioning_script.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# Simple script for provisioning a virtual machine with puppet
+
+PUPPET=`which puppet`
+UNZIP=`which unzip`
+
+fail_and_exit() {
+	echo "Provisioning failed"
+	exit 1
+}
+
+pushd /root
+  # extract our puppet files
+	$UNZIP master.zip || fail_and_exit
+
+	pushd jidoteki-examples-master/atlassian-stash/provisioning || fail_and_exit
+		# run puppet in standalone mode
+		$PUPPET apply --modulepath=modules/ manifests/site.pp || fail_and_exit
+	popd
+popd
+
+echo "Provisioning completed successfully"
+exit 0
